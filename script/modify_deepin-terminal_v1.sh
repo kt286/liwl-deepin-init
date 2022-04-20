dfe83fff1fe479a366b7f2746f9c46565a2073a9
木一明 <yunweinote@126.com>
2022-04-20 12:15:29 +0800

    修改后统一提交

--- script/modify_deepin-terminal_v1.sh
+++ script/modify_deepin-terminal_v1.sh
@@ -20,4 +20,5 @@ sudo cp ${CONFIG_DIR}/deepin-terminal/Dark.colorscheme /usr/share/terminalwidget
 sudo cp ${CONFIG_DIR}/deepin-terminal/Light.colorscheme /usr/share/terminalwidget5/color-schemes/
 
 #拷贝系统库
-sudo cp ${CONFIG_DIR}/deepin-terminal/libterminalwidget5.so.0.14.1 /lib/x86_64-linux-gnu/
+sudo cp ${CONFIG_DIR}/deepin-terminal/libterminalwidget5.so.0.14.1 /lib/x86_64-linux-gnu/libterminalwidget5.so.0.14.1.liwl
+sudo ln -fs /lib/x86_64-linux-gnu/libterminalwidget5.so.0.14.1.liwl /lib/x86_64-linux-gnu/libterminalwidget5.so.0.14.1
