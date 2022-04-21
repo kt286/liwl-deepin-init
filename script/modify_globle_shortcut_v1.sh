9c97d8155f9c36f30a50608e3f6e8f6f2d5778e8
木一明 <yunweinote@126.com>
2022-04-21 10:43:35 +0800

    增加微信全局快捷键

--- script/modify_globle_shortcut_v1.sh
+++ script/modify_globle_shortcut_v1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 #删除全局
-#设置super+1打开edge
+#设置super+1打开Edge浏览器
 busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding com.deepin.daemon.Keybinding DeleteShortcutKeystroke sis "switch-to-workspace-1" 3 "<Super>1"
 busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding com.deepin.daemon.Keybinding AddCustomShortcut sss "edge" "/opt/microsoft/msedge/msedge" "<Super>1"
 
@@ -8,3 +9,7 @@ busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding co
 busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding com.deepin.daemon.Keybinding DeleteShortcutKeystroke sis "switch-to-workspace-2" 3 "<Super>2"
 busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding com.deepin.daemon.Keybinding ModifiedAccel sisb "terminal" 0 "<Super>2" true
 gsettings set com.deepin.dde.keybinding.system terminal "['<Super>2']" #控制中心现实
+
+#设置Alt+w打开微信wechat
+busctl --user call com.deepin.daemon.Keybinding /com/deepin/daemon/Keybinding com.deepin.daemon.Keybinding AddCustomShortcut sss "wechat" "/opt/deepinwine/tools/sendkeys.sh w WeChat
+" "<Alt>w"
